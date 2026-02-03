5b0cad4c7f9f550f077c99456c588348ae1f72ff
alerusso <alessandro.russo.frc@gmail.com>
2026-02-03 19:25:35 +0100

    tester update

--- tester/srcs_cpp/tester.hpp
+++ tester/srcs_cpp/tester.hpp
@@ -1,6 +1,40 @@
 #ifndef TESTER_HPP
 # define TESTER_HPP
 
+# include <iostream>
+# include <fstream>
+# include <ostream>
+# include <sstream>
+# include <string>
+# include <poll.h>
+# include <sys/unistd.h>
+# include <errno.h>
+# include <sys/socket.h>
+# include <sys/epoll.h>
+# include <sys/poll.h>
+# include <netinet/in.h>
+# include <unistd.h>
+# include <vector>
+# include <algorithm>
+# include <map>
+# include <cstring>
+# include <cstdlib>
+# include <sstream>
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <fcntl.h>
+# include <ctime>
 
+# include "../../../includes/ether.hpp"
+
+# define PROG_NAME "webserv"
+# define PROG_PATH ".."
+
+void	run_cmd(char *const argv[], std::string &output, int second);
+void	erase_space(std::string &s);
+int		update_result(std::string file, std::string &output);
+int		compare_result(std::string file, std::string &output);
 
 #endif
--- tester/srcs_cpp/utils_dir/utils.cpp
+++ tester/srcs_cpp/utils_dir/utils.cpp
@@ -1,1 +1,122 @@
-void	utils(){}
+#include "../tester.hpp"
+
+void	run_cmd(char *const argv[], std::string &output, int second);
+
+void	run_cmd(char *const argv[], std::string &output, int second)
+{
+	int					pipe_fd[2];
+
+	pid_t	pid;
+	if (pipe(pipe_fd) != 0)
+		return (std::cout << "run_script fatal error\n", (void)0);
+	pid = fork();
+	if (pid == -1)
+		return (std::cout << "run_script fatal error\n", (void)0);
+	else if (pid == 0)
+	{
+		dup2(pipe_fd[1], STDOUT_FILENO);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		//free
+		execve(argv[0], argv, NULL);
+		std::cerr << "run_cmd fatal error\n";
+		std::exit(1);
+	}
+	close(pipe_fd[1]);
+	sleep(second);
+	kill(pid, SIGINT);
+	std::string	filename("/dev/fd/" + ft_to_string(pipe_fd[0]));
+	std::cout << filename << std::endl;
+	std::ifstream	output_fd(filename.c_str(), std::ios_base::in);
+	std::getline(output_fd, output, '\0');
+	close(pipe_fd[0]);
+}
+
+void	erase_space(std::string &s)
+{
+	size_t	i;
+	size_t	j;
+
+	i = s.find_first_of(" \f\n\r\t\v");
+	while (i != s.npos)
+	{
+		j = s.find_first_not_of(" \f\n\r\t\v", i);
+		if (j == s.npos)
+			j = s.length();
+		j -= i;
+		s.erase(i, j);
+		i = s.find_first_of(" \f\n\r\t\v", i);
+	}
+}
+
+static int	get_file_input(std::string filename, std::string &input);
+static int	get_file_output(std::string filename, std::string &output);
+
+int		update_result(std::string filename, std::string &output)
+{
+	std::fstream	fd;
+	std::string		temp;
+	std::string		file_input;
+
+	if (get_file_input(filename, file_input) != 0)
+		return (1);
+	fd.open(filename, std::ios_base::trunc);
+	if (fd.fail())
+	{
+		std::cerr << "cannot update " << filename << std::endl;
+		return (1);
+	}
+	fd << file_input << output;
+	return (0);
+}
+
+int		compare_result(std::string filename, std::string &output)
+{
+	std::fstream	fd;
+	std::string		temp;
+	std::string		file_input;
+	std::string		file_output;
+
+	if (get_file_output(filename, file_output) != 0)
+		return (1);
+	erase_space(file_output);
+	if (output == file_output)
+		return (0);
+	std::cout << "ERROR LOG: test " << filename << "\n";
+	std::cout << "CORRECT: " << file_output << "\n";
+	std::cout << "OUTPUT: " << output << "\n";
+	return (2);
+}
+
+static int	get_file_input(std::string filename, std::string &input)
+{
+	std::fstream	fd(filename);
+	std::string		temp;
+
+	if (fd.fail())
+	{
+		std::cerr << "fail to open " << filename << std::endl;
+		return (1);
+	}
+	std::getline(fd, temp, '!');
+	input += temp;
+	std::getline(fd, temp, '\n');
+	input += temp;
+	return (0);
+}
+
+static int	get_file_output(std::string filename, std::string &output)
+{
+	std::fstream	fd(filename);
+	std::string		temp;
+
+	if (fd.fail())
+	{
+		std::cerr << "fail to open " << filename << std::endl;
+		return (1);
+	}
+	std::getline(fd, temp, '!');
+	std::getline(fd, temp, '\n');
+	std::getline(fd, output, '\0');
+	return (0);
+}
